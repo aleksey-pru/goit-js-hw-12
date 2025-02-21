e8e1fc792593ab28aa973239837b87c766012de0
Oleksii <o.pruzhanskiy@gmail.com>
2025-02-21 13:38:03 +0200

    fix scroll

--- src/main.js
+++ src/main.js
@@ -5,17 +5,22 @@ import "izitoast/dist/css/iziToast.min.css";
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
-
-const form = document.querySelector('.submit-form');
-const galleryContainer = document.querySelector('.gallery');
-const loader = document.querySelector('.loader');
-const loadbtn = document.querySelector('.load-btn');
+const refs = {
+    form : document.querySelector('.submit-form'),
+    galleryContainer : document.querySelector('.gallery'),
+    loader : document.querySelector('.loader'),
+    loadbtn : document.querySelector('.load-btn'),
+}
+// const form = document.querySelector('.submit-form');
+// const galleryContainer = document.querySelector('.gallery');
+// const loader = document.querySelector('.loader');
+// const loadbtn = document.querySelector('.load-btn');
 
 export let page = 1;
 export let per_page = 40; 
 let search = '';
 
-form.addEventListener('submit', async e => {
+refs.form.addEventListener('submit', async e => {
     e.preventDefault();
     search =  e.target.elements.searchInput.value.trim();
     if (!search) {
@@ -26,13 +31,13 @@ form.addEventListener('submit', async e => {
         });
         return;
     }
-    loader.classList.remove('hidden');
-    galleryContainer.innerHTML = '';
+    refs.loader.classList.remove('hidden');
+    refs.galleryContainer.innerHTML = '';
     page = 1;
 
     try {
         const response = await fetchSearch(search, page, per_page);
-        form.reset();
+        refs.form.reset();
         const imagesData = response.data.hits;
         if(imagesData.length === 0) {
             iziToast.error({
@@ -42,7 +47,7 @@ form.addEventListener('submit', async e => {
             });  
             return;      
     }
-    renderGallery(imagesData, galleryContainer);
+    renderGallery(imagesData, refs.galleryContainer);
     page += 1;
 
     } catch (error) {
@@ -53,31 +58,34 @@ form.addEventListener('submit', async e => {
         });
         console.log(error);
     } finally {
-        loader.classList.add('hidden');
-        loadbtn.classList.remove('hidden');
+        refs.loader.classList.add('hidden');
+        refs.loadbtn.classList.remove('hidden');
     }
 
 
 })
 
-loadbtn.addEventListener('click', async e => {
+refs.loadbtn.addEventListener('click', async e => {
     e.preventDefault();
-    loadbtn.classList.add('hidden');
-    loader.classList.remove('hidden');
+    refs.loadbtn.classList.add('hidden');
+    refs.loader.classList.remove('hidden');
     try {
         const response = await fetchSearch(search, page, per_page);
         const imagesData = response.data.hits;
         const totalHits = response.data.totalHits;
-        renderGallery(imagesData, galleryContainer, true);
+        renderGallery(imagesData, refs.galleryContainer, true);
         page += 1;
+        scrollPage();
         const totalPages = page * per_page;
         if (totalPages >= totalHits) {
-            loadbtn.classList.add('hidden');
+            refs.loadbtn.classList.add('hidden');
             iziToast.info({
                 position: 'topRight',
                 title: 'Info',
                 message: "We're sorry, but you've reached the end of search results.",
             });
+        } else {
+            refs.loadbtn.classList.remove('hidden');
         }
     } catch (error) {
         iziToast.error({
@@ -87,8 +95,16 @@ loadbtn.addEventListener('click', async e => {
         });
         console.log(error);
     } finally {
-        loader.classList.add('hidden');
-        loadbtn.classList.remove('hidden');
+        refs.loader.classList.add('hidden');
     }
 
 })
+
+function scrollPage(){ 
+    const info = refs.galleryContainer.firstElementChild.getBoundingClientRect();
+    const height = info.height;
+    scrollBy({
+        'behavior': "smooth",
+        'top': height * 3,
+    })
+}
